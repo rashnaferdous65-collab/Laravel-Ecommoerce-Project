1796a8df5d93451667d030133926606fcf6f72c2
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-08-15 22:30:41 +0600

    Add order model

---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'name',
+        'rec_address',
+        'phone',
+        'user_id',
+        'product_id',
+        'quantity',
+        'payment_status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+}

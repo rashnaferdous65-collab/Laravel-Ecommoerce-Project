8aeac26875afd09efb90f2dd84154cc2e76691e0
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-08-20 23:15:01 +0600

    Update forgot password view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,22 +1,39 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="mb-5 text-sm text-gray-600">
+        {{ __('Forgot your password? No problem. Enter your email address below and we will send you a link to reset your password.') }}
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <x-auth-session-status
+        class="mb-4"
+        :status="session('status')"
+    />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form action="{{ route('password.email') }}" method="POST">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="space-y-2">
+            <x-input-label
+                for="email"
+                :value="__('Email')"
+            />
+
+            <x-text-input
+                id="email"
+                name="email"
+                type="email"
+                class="block w-full"
+                :value="old('email')"
+                required
+                autofocus
+            />
+
+            <x-input-error
+                :messages="$errors->get('email')"
+                class="mt-2"
+            />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex justify-end mt-5">
             <x-primary-button>
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>

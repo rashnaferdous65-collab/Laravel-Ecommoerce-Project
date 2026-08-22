68a196784b1d41999fb6d3d40174e4b9c3ee04f1
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-08-22 22:07:46 +0600

    Update register authentication view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,88 +1,163 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 @include('home.css')
 
 <body>
- 
-@include('home.header')
- 
 
+    @include('home.header')
 
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+    <x-guest-layout>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="space-y-4">
 
-           <!-- Phone -->
-        <div class="mt-4">
-            <x-input-label for="phone" :value="__('Phone')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="tel" name="phone" :value="old('phone')" required autocomplete="phone" />
-            <x-input-error :messages="$errors->get('phone')" class="mt-2" />
-        </div>
+                {{-- Name --}}
+                <div>
+                    <x-input-label for="name" :value="__('Name')" />
 
-        
-        <!--  Address -->
-        <div class="mt-4">
-            <x-input-label for="address" :value="__('Address')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autocomplete="address" />
-            <x-input-error :messages="$errors->get('address')" class="mt-2" />
-        </div>
+                    <x-text-input
+                        id="name"
+                        name="name"
+                        type="text"
+                        class="block w-full mt-1"
+                        :value="old('name')"
+                        required
+                        autofocus
+                        autocomplete="name"
+                    />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                    <x-input-error
+                        :messages="$errors->get('name')"
+                        class="mt-2"
+                    />
+                </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                {{-- Email --}}
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                    <x-text-input
+                        id="email"
+                        name="email"
+                        type="text"
+                        class="block w-full mt-1"
+                        :value="old('email')"
+                        required
+                        autocomplete="username"
+                    />
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                    <x-input-error
+                        :messages="$errors->get('email')"
+                        class="mt-2"
+                    />
+                </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                {{-- Phone --}}
+                <div>
+                    <x-input-label for="phone" :value="__('Phone')" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                    <x-text-input
+                        id="phone"
+                        name="phone"
+                        type="tel"
+                        class="block w-full mt-1"
+                        :value="old('phone')"
+                        required
+                        autocomplete="tel"
+                    />
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+                    <x-input-error
+                        :messages="$errors->get('phone')"
+                        class="mt-2"
+                    />
+                </div>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                {{-- Address --}}
+                <div>
+                    <x-input-label for="address" :value="__('Address')" />
 
+                    <x-text-input
+                        id="address"
+                        name="address"
+                        type="text"
+                        class="block w-full mt-1"
+                        :value="old('address')"
+                        required
+                        autocomplete="street-address"
+                    />
 
-   
+                    <x-input-error
+                        :messages="$errors->get('address')"
+                        class="mt-2"
+                    />
+                </div>
 
-  <!-- info section -->
+                {{-- Password --}}
+                <div>
+                    <x-input-label for="password" :value="__('Password')" />
 
-@include('home.footer')
+                    <x-text-input
+                        id="password"
+                        name="password"
+                        type="password"
+                        class="block w-full mt-1"
+                        required
+                        autocomplete="new-password"
+                    />
+
+                    <x-input-error
+                        :messages="$errors->get('password')"
+                        class="mt-2"
+                    />
+                </div>
+
+                {{-- Confirm Password --}}
+                <div>
+                    <x-input-label
+                        for="password_confirmation"
+                        :value="__('Confirm Password')"
+                    />
+
+                    <x-text-input
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        class="block w-full mt-1"
+                        required
+                        autocomplete="new-password"
+                    />
+
+                    <x-input-error
+                        :messages="$errors->get('password_confirmation')"
+                        class="mt-2"
+                    />
+                </div>
+
+            </div>
+
+            <div class="flex items-center justify-end mt-6">
+
+                <a
+                    href="{{ route('login') }}"
+                    class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-primary-button class="ms-4">
+                    {{ __('Register') }}
+                </x-primary-button>
+
+            </div>
+
+        </form>
+
+    </x-guest-layout>
+
+    @include('home.footer')
 
 </body>
 

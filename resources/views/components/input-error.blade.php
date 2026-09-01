0791c185e1adc40bf1ab545034257b6698ab1a9f
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-09-01 22:09:12 +0600

    Update input error component

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,14 @@
+```blade
 @props(['messages'])
 
-@if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
-        @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+@if (!empty($messages))
+    <ul {{ $attributes->merge([
+        'class' => 'text-sm text-red-600 space-y-1'
+    ]) }}>
+        @foreach ((array) $messages as $error)
+            <li>{{ $error }}</li>
         @endforeach
     </ul>
 @endif
+```
+

0634114e4b68cffbbd227410114cf2ae2258ab2f
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-09-13 23:14:29 +0600

    Update secondary button component

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,11 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+```blade
+<button
+    {{ $attributes->merge([
+        'type' => 'button',
+        'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md text-xs font-semibold text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition duration-150 ease-in-out'
+    ]) }}
+>
     {{ $slot }}
 </button>
+```
+
